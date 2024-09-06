b3e57e24b3c97ba358509b419ffa1e856d1e3bb4
Britnay Gore <britnaygore268@gmail.com>
2024-09-05 22:28:02 -0400

    Created data models for artists, songs, and genres

---
+++ Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TunaPiano_BE.Models;
+
+public class Genre
+{
+	public int Id { get; set; }
+	public string? Description { get; set; }
+	List<Song> Songs { get; set; }
+}
+
